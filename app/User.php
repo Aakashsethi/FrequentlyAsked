8e39a052afd913974cac60cab760778c531c4394
Aakash Sethi <as3793@njit.edu>
2019-04-28 06:32:38 -0700

    Task:Added User Profile migrate, model, and update the user seeder to create profiles

--- app/User.php
+++ app/User.php
@@ -34,7 +34,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -43,7 +42,6 @@ class User extends Authenticatable
     protected $fillable = [
         'email', 'password',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -52,13 +50,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
 }
