256951696a44146c6e4221833328d911d0182d0f
Aakash Sethi <as3793@njit.edu>
2019-05-15 19:25:49 -0700

    sendgrid configuration

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.16 on 2019-05-14 01:12:30.
+ * Generated for Laravel 5.8.17 on 2019-05-16 02:02:12.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
